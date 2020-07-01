c634e5408da0ee0ec3c9e3ea6413806851e2675b
Petr Veigend <iveigend@fit.vutbr.cz>
2020-07-01 11:42:54 +0200

    update for xml parser - additional units

--- ruian/xml/parser.py
+++ ruian/xml/parser.py
@@ -1,7 +1,28 @@
 import xml.etree.ElementTree as ET
+import pyproj
+
+sjtsk = pyproj.Proj("+init=epsg:5514")
+wgs = pyproj.Proj("+init=epsg:4326")
 
 staty = []
 kraje = []
+okresy = []
+obce = []
+
+
+# obce
+obecSRozsirenouPusobnosti = 1
+obecSPoverenymObecnimUradem = 2
+obec = 3
+
+def toGPS(s):
+    c = s.split(' ')
+    cc = pyproj.transform(sjtsk, wgs, float(c[0]), float(c[1]))
+    coords = {}
+    coords['lat'] = cc[1]
+    coords['lon'] = cc[0]
+    return coords    
+
 
 tree = ET.parse('ruian.xml')
 root = tree.getroot()
@@ -15,7 +36,7 @@ stat['nazev'] = children[3].text
 staty.append(stat)
 
 #print(children)
-
+# vyssi spravni celky - kraje
 data = root[1][3]
 children = data.iter()
 kraj = {}
@@ -24,11 +45,67 @@ for ch in children:
         if kraj:
             kraje.append(kraj)
         kraj = {}
-        kraj['okresy'] = [] 
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
         kraj['id'] = ch.text
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Nazev':
         kraj['nazev'] = ch.text
 
 
+# okresy
+data = root[1][4]
+children = data.iter()
+okres = {}
+for ch in children:
+#    print(ch.tag)
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Okres':
+        if okres:
+            okresy.append(okres)
+        okres = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OkresIntTypy:v1}Kod':
+        okres['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OkresIntTypy:v1}Nazev':
+        okres['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
+        okres['kraj'] = ch.text
 
+# obce s rozsirenou pusobnosti
+data = root[1][5]
+children = data.iter()
+obec = {}
+obec['typ'] = obecSRozsirenouPusobnosti
+for ch in children:
+    #print(ch.tag)
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Orp':
+        if obec:
+            obce.append(obec)
+        obec = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OrpIntTypy:v1}Kod':
+        obec['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OrpIntTypy:v1}Nazev':
+        obec['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
+        obec['kraj'] = ch.text
+    if ch.tag == '{http://www.opengis.net/gml/3.2}pos':
+        obec['pos'] = ''#toGPS(ch.text)
+
+print(len(obce))
+
+# obce s poverenym obecnim uradem
+data = root[1][6]
+children = data.iter()
+obec = {}
+obec['typ'] = obecSPoverenymObecnimUradem
+for ch in children:
+    #print(ch.tag)
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Pou':
+        if obec:
+            obce.append(obec)
+        obec = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Kod':
+        obec['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Nazev':
+        obec['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
+        obec['kraj'] = ch.text
+
+print(len(obce))
