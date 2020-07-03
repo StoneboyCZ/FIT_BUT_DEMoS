4fc7bb5f40c77457b5769cadb5eed6f706467ff1
Petr Veigend <iveigend@fit.vut.cz>
2020-07-03 12:32:03 +0200

    finished json parsing of RUIAN data

--- ruian/xml/parser.py
+++ ruian/xml/parser.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import pyproj
+import json
 
 sjtsk = pyproj.Proj("+init=epsg:5514")
 wgs = pyproj.Proj("+init=epsg:4326")
@@ -7,7 +8,10 @@ wgs = pyproj.Proj("+init=epsg:4326")
 staty = []
 kraje = []
 okresy = []
+obceSRozsirenouPusobnosti = []
+obceSPoverenymObecnimUradem = []
 obce = []
+casti = []
 
 def toGPS(s):
     c = s.split(' ')
@@ -17,6 +21,9 @@ def toGPS(s):
     coords['lon'] = cc[0]
     return coords    
 
+def saveJSON(fn,data):
+    with open(fn+'.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 tree = ET.parse('ruian.xml')
 root = tree.getroot()
@@ -29,12 +36,14 @@ stat['id'] = children[2].text
 stat['nazev'] = children[3].text 
 staty.append(stat)
 
+saveJSON('staty',staty)
+
 #print(children)
 # vyssi spravni celky - kraje
 data = root[1][3]
-children = data.iter()
+children = list(data.iter())
 kraj = {}
-for ch in children:
+for i,ch in enumerate(children):
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Vusc':
         if kraj:
             kraje.append(kraj)
@@ -44,13 +53,17 @@ for ch in children:
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Nazev':
         kraj['nazev'] = ch.text
 
+    if i == len(children)-1:
+        kraje.append(kraj)
+
+saveJSON('kraje',kraje)
+
 
 # okresy
 data = root[1][4]
-children = data.iter()
+children = list(data.iter())
 okres = {}
-for ch in children:
-#    print(ch.tag)
+for i,ch in enumerate(children):
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Okres':
         if okres:
             okresy.append(okres)
@@ -61,21 +74,112 @@ for ch in children:
         okres['nazev'] = ch.text
     if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
         okres['kraj'] = ch.text
+    
+    if i == len(children) -1:
+        okresy.append(okres)    
+
+saveJSON('okresy',okresy)
+
+# obce s rozsirenou pusobnosti
+data = root[1][5]
+children = list(data.iter())
+obecSRozsirenouPusobnosti = {}
+for i,ch in enumerate(children):
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Orp':
+        if obecSRozsirenouPusobnosti:
+            obceSRozsirenouPusobnosti.append(obecSRozsirenouPusobnosti)
+        obecSRozsirenouPusobnosti = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OrpIntTypy:v1}Kod':
+        obecSRozsirenouPusobnosti['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OrpIntTypy:v1}Nazev':
+        obecSRozsirenouPusobnosti['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
+        obecSRozsirenouPusobnosti['kraj'] = ch.text
+    if ch.tag == '{http://www.opengis.net/gml/3.2}pos':
+        obecSRozsirenouPusobnosti['pos'] = toGPS(ch.text)
+
+    if i == len(children)-1:
+        obceSRozsirenouPusobnosti.append(obecSRozsirenouPusobnosti)    
+
+saveJSON('obceSRozsirenouPusobnosti',obceSRozsirenouPusobnosti)
+
+# obce s poverenym obecnim uradem
+data = root[1][6]
+children = list(data.iter())
+obecSPoverenymObecnimUradem = {}
+for i,ch in enumerate(children):
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Pou':
+        if obecSPoverenymObecnimUradem:
+            obceSPoverenymObecnimUradem.append(obecSPoverenymObecnimUradem)
+        obecSPoverenymObecnimUradem = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Kod':
+        obecSPoverenymObecnimUradem['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Nazev':
+        obecSPoverenymObecnimUradem['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OrpIntTypy:v1}Kod':
+        obecSPoverenymObecnimUradem['orp'] = ch.text
+    if ch.tag == '{http://www.opengis.net/gml/3.2}pos':
+        obecSPoverenymObecnimUradem['pos'] = toGPS(ch.text)
+
+    if i == len(children)-1:
+        obceSPoverenymObecnimUradem.append(obecSPoverenymObecnimUradem)    
+
+saveJSON('obceSPoverenymObecnimUradem',obceSPoverenymObecnimUradem)
 
 # obce 
 data = root[1][7]
-children = data.iter()
+children = list(data.iter())
 obec = {}
-for ch in children:
-    print(ch.tag)
-    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Pou':
+for i,ch in enumerate(children):
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Obec':
         if obec:
             obce.append(obec)
         obec = {}
-    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Kod':
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:ObecIntTypy:v1}Kod':
         obec['id'] = ch.text
-    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Nazev':
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:ObecIntTypy:v1}Nazev':
         obec['nazev'] = ch.text
-    if ch.tag == '{urn:cz:isvs:ruian:schemas:VuscIntTypy:v1}Kod':
-        obec['kraj'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:OkresIntTypy:v1}Kod':
+        obec['okres'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:PouIntTypy:v1}Kod':
+        obec['pou'] = ch.text
+    if ch.tag == '{http://www.opengis.net/gml/3.2}pos':
+        obec['pos'] = toGPS(ch.text)
+    
+    if i == len(children)-1:
+        obce.append(obec)  
 
+saveJSON('obce',obce)
+
+# casti mest a obci
+data1 = root[1][10]
+children1 = list(data1.iter())
+data2 = root[1][11]
+children2 = list(data2.iter())
+children = children1+children2
+cast = {}
+for i,ch in enumerate(children):
+    #print(ch.tag)
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}Momc': 
+        if cast:
+            cast['typ'] = 'castMesta'
+            casti.append(cast)
+        cast = {}
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:VymennyFormatTypy:v1}CastObce':
+        if cast:
+            cast['typ'] = 'castObce'
+            casti.append(cast)
+        cast = {} 
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:MomcIntTypy:v1}Kod' or ch.tag == '{urn:cz:isvs:ruian:schemas:CastObceIntTypy:v1}Kod':
+        cast['id'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:MomcIntTypy:v1}Nazev' or ch.tag == '{urn:cz:isvs:ruian:schemas:CastObceIntTypy:v1}Nazev':
+        cast['nazev'] = ch.text
+    if ch.tag == '{urn:cz:isvs:ruian:schemas:ObecIntTypy:v1}Kod' or ch.tag == '{urn:cz:isvs:ruian:schemas:ObecIntTypy:v1}Kod':
+        cast['obec'] = ch.text
+    if ch.tag == '{http://www.opengis.net/gml/3.2}pos':
+        cast['pos'] = toGPS(ch.text)
+    
+    if i == len(children)-1:
+        casti.append(cast)
+
+saveJSON('castiObci',casti)
